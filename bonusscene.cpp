46940a7e53e9358b76dc2e9047be745c09174e53
kgmayumi <mayumi.kitade@gamil.com>
2020-08-12 14:48:08 +0900

    ボーナスシーン、フェード処理、サウンドの追加

---
+++ bonusscene.cpp
@@ -0,0 +1,68 @@
+//! @file	bonusscene.cpp
+//! @author	kitade mayumi
+//! @date	2020-08-09
+//! @brief	ボーナスシーンの実装
+
+// インクルードファイル
+#include "main.h"
+#include "scene.h"
+#include "bonusscene.h"
+#include "file.h"
+#include "playerTest.h"
+#include "item.h"
+#include "bg.h"
+#include "map01.h"
+#include "score.h"
+#include "timer.h"
+#include "life.h"
+#include "effect.h"
+#include "sound.h"
+
+// シーンの初期化処理
+void InitBonusScene(void)
+{
+	OpenBonusFile();
+	InitBg(BONUS_BG);
+	InitMap();
+	InitItem(0);
+	InitPlayer();
+	InitEffect(0);
+	PlaySound(SOUND_LABEL_BGM_sample002);
+}
+
+// シーンの終了処理
+void UninitBonusScene(void)
+{
+	UninitEffect();
+	UninitMap();
+	UninitBg();
+	UninitItem();
+	UninitPlayer();
+	StopSound(SOUND_LABEL_BGM_sample002);
+}
+
+// シーンの更新処理
+void UpdateBonusScene(void)
+{
+	UpdateMap();
+	UpdatePlayer();
+	UpdateBg();
+	UpdateItem();
+	UpdateEffect();
+	UpdateScore();
+	UpdateLife();
+	UpdateTimer();
+}
+
+// シーンの描画処理
+void DrawBonusScene(void)
+{
+	DrawBg();
+	DrawMap();
+	DrawScore();
+	DrawLife();
+	DrawTimer();
+	DrawItem();
+	DrawPlayer();
+	DrawEffect();
+}
---
+++ bonusscene.h
@@ -0,0 +1,11 @@
+//! @file	bonusscene.h
+//! @author	kitade mayumi
+//! @date	2020-08-09
+//! @brief	ボーナスシーンの定義
+#pragma once
+
+// プロトタイプ宣言
+void InitBonusScene(void);
+void UninitBonusScene(void);
+void UpdateBonusScene(void);
+void DrawBonusScene(void);
